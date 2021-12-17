07e42e134cc27682ed54030f8ea698fc822feaae
Alex <opetrovskyi@mggpaero.com>
2021-12-17 14:00:55 +0100

    start valid

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from './Input.module.css'
-const Input = ({label,placeholder,required}) => {
+const Input = ({label,placeholder,required, value, readOnly}) => {
     return (
         <div>
             <div>
                 <label>
                     {label} {required && <span>*</span>}
                 </label>
-                <input className={styled.input_input} type="text" name="" placeholder={placeholder} />
+                <input className={styled.input_input} type="text" name="" placeholder={placeholder} value={value} readOnly={readOnly}  />
             </div>
         </div>
     );
--- src/components/pages/ProjectAddPage/ProjectAddPage.js
+++ src/components/pages/ProjectAddPage/ProjectAddPage.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Layouts from "../../molekuls/Layouts/Layouts";
 import Input from "../../atoms/Input/Input";
 import styled from './ProjectAddPage.module.css'
 import {Link} from "react-router-dom";
+import {jsonOb} from "../../../env";
 
 const ProjectAddPage = () => {
+    const [data, setData] = useState([])
+
+    new Promise(async (resolve, reject) => resolve(jsonOb))
+        .then(value => {
+            const result = value.response.data
+            setData(result)
+        })
+
+    useEffect(()=>{
+        new Promise(async (resolve, reject) => resolve(jsonOb))
+            .then(value => {
+                const result = value.response.data
+                setData(result)
+            })
+    },[data])
     return (
         <div>
             <Layouts>
@@ -16,7 +32,12 @@ const ProjectAddPage = () => {
 
                         <div className={styled.wrapInputs}>
                             <div className={styled.leftInput}>
-                                <Input required label={'Name'} placeholder={'NameProject'}/>
+                                <Input
+                                    value={data.length +1}
+                                    readOnly={true}
+                                    required
+                                    label={'Numer Projektu'}
+                                    placeholder={'Numer Projektu'}/>
                             </div>
                             <div className={styled.rightInput}>
                                 <Input required label={'Name2'} placeholder={'NameProject2'}/>
