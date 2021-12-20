698c9f5e583b829b71e6b8a9e4a7c1f745072bec
Alex <opetrovskyi@mggpaero.com>
2021-12-20 10:25:14 +0100

    add loginPage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import MainPage from "./components/pages/MainPage/MainPage";
 import ProjectPage from "./components/pages/ProjectPage/ProjectPage";
 import ProjectAddPage from "./components/pages/ProjectAddPage/ProjectAddPage";
+import LoginPage from "./components/pages/LoginPage/LoginPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
              <Route path="/" component={MainPage} exact />
              <Route path="/project/:id" component={ProjectPage} />
              <Route path="/project-add" component={ProjectAddPage} />
+             <Route path="/login" component={LoginPage} />
          </Switch>
      </Router>
 
--- src/components/molekuls/Header/Header.js
+++ src/components/molekuls/Header/Header.js
@@ -3,22 +3,33 @@ import styled from './Header.module.css'
 import logo from './logo.svg'
 
 import {Link} from "react-router-dom";
+
 const Header = () => {
-    const handleChange = (e)=>{
+    const handleChange = (e) => {
         console.log(e)
     }
     return (
         <div>
             <header className={styled.header}>
-                <div className={ styled.header_container }>
+                <div className={styled.header_container}>
 
                     <div className={styled.header_column}>
                         <div className={styled.header_left}>
                             <Link style={{textDecoration: 'none'}} to='/'>
-                                <div className={styled.header_logo} >
+                                <div className={styled.header_logo}>
                                     <img src={logo} alt="logo"/>
                                 </div>
                             </Link>
+                        </div>
+                        <div className={styled.header_right}>
+
+                            <div className={styled.header_right_logout}>
+                                <Link to={'/login'}>
+                                   <button className={styled.logoutBtn}>logout &#10153; </button>
+                                </Link>
+                            </div>
+
+
                         </div>
                     </div>
 
